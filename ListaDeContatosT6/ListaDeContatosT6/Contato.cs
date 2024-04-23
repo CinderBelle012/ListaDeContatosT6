1311cfb95467891982d45406e309b87673205363
CinderBelle012 <167465429+CinderBelle012@users.noreply.github.com>
2024-04-23 16:36:42 -0300

    Criada a classe
    Criada a classe cs, feito o código telefone, nome e sebrenome

---
+++ ListaDeContatosT6/ListaDeContatosT6/Contato.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeContatosT6
+{
+    internal class Contato
+    {
+        private string nome;
+        private string sobrenome;
+        private string telefone;
+
+        //Propriedades. São os metodos "get"  e "set"
+        // get = leitura
+        //set = definição
+        // é possivel ser somente leitura, ou somente definição...
+        public string Nome
+        {
+
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+
+        }
+        public string Sobrenome
+        {
+
+            get
+            {
+                return sobrenome;
+            }
+            set
+            {
+                sobrenome = value;
+            }
+
+
+        }
+        public string Telefone
+        {
+            get
+            {
+                return telefone;
+            }
+            set
+            {
+                if (value.Length == 13)
+                {
+                    telefone = value;
+                }
+                else
+                {
+                    Telefone = "(00) 0000-0000";
+                }
+            }
+        }
+
+        public Contato()
+        {
+            Nome = "Claudia";
+            Sobrenome = "Genésio II";
+            Telefone = "11 4002-8922";
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
